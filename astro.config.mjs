3ffd0fa240bcacb0f50deee110eba8939e5fac18
Neitodesu <neitodesu92@gmail.com>
2023-10-28 18:34:30 -0400

    updated yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://Neitodesu.github.io',
+	base: '/https://github.com/Neitodesu/FEM-Fylo-Data-Storage',
+});
